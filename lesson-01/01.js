bc2adef1b2900bf9c2ac1009d8f87be2f7c7f13d
KarpenkoEgor <karpenkoegor190@gmail.com>
2024-10-13 17:45:10 +0300

    hfjdj

---
+++ lesson-01/01.js
@@ -0,0 +1,22 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+// let sum = 210
+// let count = 1
+
+// while(count <= 20){
+//     sum = count * (count + 1) / 2
+//     count = count + 1
+// }
+
+let sum = 210
+let s = 1
+let count = 1
+
+while(count < 20){
+count += 1
+s += count
+console.log(s)
+}
+
