e2c67316c3aa1b09ad32c941ca2e7b915b2526c9
KarpenkoEgor <karpenkoegor190@gmail.com>
2024-10-19 18:01:38 +0300

    complete

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,25 +7,25 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1; 
-let dice2;
+let dice1 = 1; 
+let dice2 = 1;
 let isWinningDouble = false; // your code
 const keyWordsOne = ["1", "2", "3", "4", "5", "6"];
 const keyWordsTwo = ["1", "2", "3", "4", "5", "6"];
 
 while(!isWinningDouble){
-  const randomOne = Math.floor(Math.random() * 6);
-  const randomTwo = Math.floor(Math.random() * 6);
+  const randomOne = Math.floor(Math.random(1) * 6);
+  const randomTwo = Math.floor(Math.random(1) * 6);
   dice1 = keyWordsOne[randomOne];
   console.log('Первый бросок: ' + dice1);
   dice2 = keyWordsTwo[randomTwo];
   console.log('ПЕРВЫЙ бросок: ' + dice2);
   alert(dice1);
   alert(dice2) 
-if((dice1 === keyWordsOne[2] && dice2 === keyWordsTwo[2])||
-(dice1 === keyWordsOne[3] && dice2 === keyWordsTwo[3])||
+if((dice1 === keyWordsOne[3] && dice2 === keyWordsTwo[3])||
 (dice1 === keyWordsOne[4] && dice2 === keyWordsTwo[4])||
-(dice1 === keyWordsOne[5] && dice2 === keyWordsTwo[5]))
+(dice1 === keyWordsOne[5] && dice2 === keyWordsTwo[5])||
+(dice1 === keyWordsOne[6] && dice2 === keyWordsTwo[6]))
 {
   isWinningDouble = true;
   alert('Выигрышный дубль!');
