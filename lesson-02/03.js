0d1db089f2f5a94f8649082fd0c8b71932461556
KarpenkoEgor <karpenkoegor190@gmail.com>
2024-10-20 14:32:11 +0300

    complete task

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,21 +4,27 @@
  * Результат сохраните в переменной grade.
  */
 
-let score  // тестовое значение, можно изменять
-let grade = ['A', 'B', 'C', 'D', 'F']
-// your code
+// let score  // тестовое значение, можно изменять
+// let grade = ['A', 'B', 'C', 'D', 'F']
+// // your code
 
-score = +prompt("Напишите ваш балл");
-if(score >= 0 && score <= 49){
-    console.log(`Ваша оценка: ${grade[4]}`);
-} else if(score >= 50 && score <= 69){
-    console.log(`Ваша оценка: ${grade[3]}`);
-} else if(score >= 70 && score <= 79){
-    console.log(`Ваша оценка: ${grade[2]}`);
-} else if(score >= 80 && score <= 89){
-    console.log(`Ваша оценка: ${grade[1]}`);
-} else if(score >= 90 && score <= 100){
-    console.log(`Ваша оценка: ${grade[0]}`);
-} else{
-    console.log('Введите корректные значения')
-}
+// score = +prompt("Напишите ваш балл");
+// if(score >= 0 && score <= 49){
+//     console.log(`Ваша оценка: ${grade[4]}`);
+//     alert(`Ваша оценка: ${grade[4]}`)
+// } else if(score >= 50 && score <= 69){
+//     console.log(`Ваша оценка: ${grade[3]}`);
+//     alert(`Ваша оценка: ${grade[3]}`)
+// } else if(score >= 70 && score <= 79){
+//     console.log(`Ваша оценка: ${grade[2]}`);
+//     alert(`Ваша оценка: ${grade[2]}`)
+// } else if(score >= 80 && score <= 89){
+//     console.log(`Ваша оценка: ${grade[1]}`);
+//     alert(`Ваша оценка: ${grade[1]}`)
+// } else if(score >= 90 && score <= 100){
+//     console.log(`Ваша оценка: ${grade[0]}`);
+//     alert(`Ваша оценка: ${grade[0]}`)
+// } else{
+//     console.log('Введите корректные значения')
+//     alert(`Введите корректное значение`)
+// }
